7438984e8362c287ee3760cfdfb31abb3b109b8c
mharnoufi <mharnoufi@outlook.com>
2025-07-08 15:41:48 +0200

    partie 4 bonus

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Avg, Count
+from .models import JobRecord
 
-# Create your views here.
+def dashboard_view(request):
+    total_jobs = JobRecord.objects.count()
+    average_salary = JobRecord.objects.aggregate(Avg('salary_in_usd'))['salary_in_usd__avg']
+    total_countries = JobRecord.objects.values('employee_residence').distinct().count()
+
+    context = {
+        'total_jobs': total_jobs,
+        'average_salary': round(average_salary, 2) if average_salary else 0,
+        'total_countries': total_countries,
+    }
+    return render(request, 'jobs/dashboard.html', context)
