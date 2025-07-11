91471582e9efd1e544b147a4fecb41c277e78a69
mharnoufi <mharnoufi@outlook.com>
2025-07-11 17:39:38 +0200

    fix api url conflit

--- feedback/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Feedback
-
-@admin.register(Feedback)
-class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ('job', 'author', 'rating', 'created_at')
-    list_filter = ('rating', 'created_at')
--- feedback/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Feedback
-
-class FeedbackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Feedback
-        fields = ['id', 'job', 'author', 'comment', 'rating', 'created_at']
